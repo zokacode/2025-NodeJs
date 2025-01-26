0d3ebd272ef6f1cbe8458f8bf81819f5bfb933ad
zoka0 <tommyhu025@gmail.com>
2025-01-27 00:43:35 +0800

    feat: add user_page

--- routes/users.js
+++ routes/users.js
@@ -2,11 +2,27 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-// router.get('/', function(req, res, next) {
-//   res.send('respond with a resource');
-// });
 router.get('/', function(req, res, next) {
-  res.render('users/index', { title: 'Users' });
+  res.send('respond with a resource');
+});
+
+router.get('/:name', function(req, res, next) {
+  let name = req.params.name || "未找到名字";
+  let limit = Number(req.query.limit) || 0;
+  let editUrl = `/users/${encodeURIComponent(name)}/edit`;
+  res.status(200).render('users/personal', { title: 'Users', name, limit, editUrl });
+});
+
+router.get('/:name/edit', function(req, res, next) {
+  let name = req.params.name || "未找到名字";
+  let editUrl = `/users/${encodeURIComponent(name)}/edit`;
+  res.status(200).render('users/personal_edit', { title: 'Users Edit', name, editUrl });
+});
+
+router.post('/:name/edit', function(req, res, next) {
+  console.log(req.body);
+  console.log(req.body.name);
+  res.status(200).send('test');
 });
 
 module.exports = router;
